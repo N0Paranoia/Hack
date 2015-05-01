0a73a77f06a48329bcf0e9fb38bd13234e250c7e
N0Paranoia <martijn.manneveld@gmail.com>
2015-05-01 16:33:27 +0200

    Ad output file for strings

---
+++ output.js
@@ -0,0 +1,40 @@
+    // commands
+    var commando01 = "\
+    					<br>\
+    					GNU bash, version 4.1.7(1)-release (i586-NoParanoia-linux-GeoBash shell)<br>\
+ 						These shell commands are defined internally.  Type \`help\' to see this list.<br>\
+ 						Use \`info geobash\' to find out more about the shell in general.<br>\
+ 						<br>\
+ 						cat [OPTION] [FILE] ................ concatenate files and print on the standard output<br>\
+ 						cd [directory] ..................... cd change the working directory<br>\
+ 						date ............................... print the system date and time<br>\
+ 						ping [remote host] ................. send ICMP ECHO_REQUEST to network hosts<br>\
+ 						ls [OPTION] [FILE] ................. list directory contents<br>\
+ 						traceroute [remote host]............ print the route packets take to package host<br>\
+ 						vi [filename] ...................... screen oriented (visual) display editor based on ex\
+ 					";
+
+    var commando02 = "\
+    					<br>\
+    					GeoBash Features<br>\
+    					****************<br>\
+    					<br>\
+						This text is a brief description of the features that are present in<br>\
+						the GeoBash shell (version 6.4, 30 February 2014).  The GeoBash home page is<br>\
+						`http://www.gnu.org/software/geobash/'.<br>\
+						<br>\
+						   This is Edition 4.3, last updated 30 February 2014, of `The GNU Bash<br>\
+						Reference Manual', for `GeoBash', Version 6.4.<br>\
+						<br>\
+						   GeoBash contains features that appear in other popular shells, and some<br>\
+						features that only appear in GeoBash.  Some of the shells that GeoBash has<br>\
+						borrowed concepts from are the Bourne Shell (`sh'), the Korn Shell<br>\
+						(`ksh'), and the C-shell (`csh' and its successor, `tcsh').\
+    				";
+
+
+
+    var commando03 = "ping";
+
+    var commando04 = "traceroute";
+    var commando05 = "exit";
